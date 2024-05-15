e537d8061e8584a55d2d8522cf4e4b758a72b2a6
Sohel Rana <sohelrana130523@gmail.com>
2024-05-15 21:02:33 +0100

    Phone hungting with API project

---
+++ JS/index.js
@@ -0,0 +1,34 @@
+const phoneHunt = async () => {
+  const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+  const data = await res.json()
+  const phoneData = data.data;
+  phoneDisplay(phoneData);
+
+}
+
+const phoneDisplay = phoneData => {
+  const phoneContainer = document.getElementById('phone-container');
+
+  phoneData.forEach(phone => {
+    const everyPhoneDiv = document.createElement('div');
+    everyPhoneDiv.classList = `card w-96 bg-base-100 shadow-xl`;
+    everyPhoneDiv.innerHTML = `
+      <figure class="px-10 pt-10">
+            <img src="${phone.image}" alt="Shoes"
+              class="rounded-xl" />
+          </figure>
+          <div class="card-body items-center text-center">
+            <h2 class="card-title">${phone.phone_name}</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions">
+              <button class="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+    `;
+
+    phoneContainer.appendChild(everyPhoneDiv);
+  });
+}
+
+
+phoneHunt();
